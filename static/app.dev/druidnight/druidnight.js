79b78b9e95fb0460c7f6f92e72957489897099ef
eglinetwork <marcoegli@gmx.ch>
2011-06-20 22:05:04 +0200

    some performance tweaks

--- static/app.dev/druidnight/druidnight.js
+++ static/app.dev/druidnight/druidnight.js
@@ -1,14 +1,3 @@
 /**
  * @author egli
  */
-
-YUI().use('node','tabview', function(Y) {
-    function main() {
-
-        Y.one('.yui3-js-enabled').removeClass('yui3-js-enabled');
-
-        Y.log("foo");
-        
-    }
-    Y.on("domready", main);
-});
